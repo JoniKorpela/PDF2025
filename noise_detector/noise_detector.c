993f7ba4efe3b41ab71efac9ade50149bbd4d9ca
mikkokko <1454476-mikkokko@users.noreply.gitlab.com>
2025-04-09 16:25:16 +0300

    add noise detector sources

---
+++ noise_detector/noise_detector.c
@@ -0,0 +1,115 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+#include "pico/stdlib.h"
+#include "hardware/gpio.h"
+#include "hardware/adc.h"
+#include "hardware/uart.h"
+#include "pico/binary_info.h"
+
+/* can be tweaked */
+#define SAMPLE_COUNT 10
+#define SAMPLE_WINDOW_MICROSECONDS (100 * 1000)
+
+/* should be tweaked */
+#define VIOLATION_THRESHOLD 0.5f
+
+#define ADC_NUM 0
+#define ADC_PIN (26 + ADC_NUM)
+#define ADC_RANGE (1 << 12)
+
+/* remap [0,4095] to [0,1] */
+#define NORMALIZE_VOLUME(adc_reading) ((adc_reading) * (1.0f / (ADC_RANGE - 1)))
+
+/* tells the kids to shut the fuck up */
+static void toot_horn(int violation_num)
+{
+}
+
+static void update_display(const char *fmt, ...)
+{
+  va_list ap;
+  char buffer[32];
+
+  va_start(ap, fmt);
+  vsnprintf(buffer, sizeof(buffer), fmt, ap);
+  va_end(ap);
+
+  /* FIXME: actually update the display */
+  puts(buffer);
+}
+
+/* coarse peak-to-peak adc voltage estimation over a 100 ms period */
+static int peak_to_peak_100ms(void)
+{
+  uint64_t start = time_us_64();
+
+  uint16_t min_sample = 4096;
+  uint16_t max_sample = 0;
+
+  while (1)
+  {
+    uint64_t current = time_us_64();
+    if (current - start >= 100000)
+    {
+      /* done */
+      break;
+    }
+
+    uint16_t sample = adc_read();
+    if (sample < min_sample) min_sample = sample;
+    if (sample > max_sample) max_sample = sample;
+  }
+
+  return max_sample - min_sample;
+}
+
+int main(void)
+{
+  stdio_init_all();
+
+  adc_init();
+  adc_gpio_init(ADC_PIN);
+  adc_select_input(ADC_NUM);
+
+  int violation_count = 0;
+
+  while (1)
+  {
+    int accum = 0;
+
+    /* sample 10 times */
+    for (int i = 0; i < SAMPLE_COUNT; i++)
+    {
+      int sample = peak_to_peak_100ms();
+      accum += sample;
+
+      update_display("Volume level: %.2f", NORMALIZE_VOLUME(sample));
+    }
+
+    /* take the average */
+    int average = accum / SAMPLE_COUNT;
+
+    /* normalize to [0-1] */
+    float loudness = NORMALIZE_VOLUME(average);
+
+    if (loudness > VIOLATION_THRESHOLD)
+    {
+      violation_count++;
+
+      /* debug prints */
+      printf("too loud (violation #%d): %.2f\n", violation_count, loudness);
+
+      toot_horn(violation_count);
+    }
+    else
+    {
+      /* debug prints */
+      printf("ok: %.2f\n", loudness);
+    }
+  }
+
+  /* never reached */
+  return 0;
+}
